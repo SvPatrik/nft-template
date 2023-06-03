c5b0d02c963c0b3ee708a6552506eff2a86a831c
SvPatrik <85416871+SvPatrik@users.noreply.github.com>
2023-06-03 18:11:15 +0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
-const nftAddress = ""; // Paste the address of the NFT collection.
+const nftAddress = "0x7f9c601807bEBB0B20CFFAD39e8D386279350aAd"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
+const chainId = "0x507"; // Moonbeam
 // const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
